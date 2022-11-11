08bda1650f12a780eb8548e06526da08f86b1e8c
Marina <mod.1@bk.ru>
2022-11-12 03:24:08 +0400

    task64

---
+++ task64/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 64: Задайте значение N. 
+// Напишите программу, которая выведет 
+// все чётные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+// N = 5 -> "4, 2"
+// N = 8 -> "8, 6, 4, 2,"
+
+void Main()
+{
+    Console.Clear();
+    Console.WriteLine("Введите число:");
+    int number=int.Parse(Console.ReadLine());
+    WriteNumber(number);
+}
+
+void WriteNumber(int number)
+{
+    if(number%2==0)
+    {
+        Console.Write(" " + number);
+    }
+    if (number > 1)
+    {
+        WriteNumber(number - 1);
+    }
+}
+Main();
+
